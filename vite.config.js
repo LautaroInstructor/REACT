8746f5342f4b68f863b933e163f65f7ef55bed03
LautaroAntriolo <lautaroantriololuis@gmail.com>
2025-09-30 10:15:56 -0300

    fix: regerar vite.config.js para deploy en Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        // Agregar estas líneas para Firebase
+        /^firebase/,
+        /^@firebase/
+      ]
+    }
+  },
+  optimizeDeps: {
+    exclude: ['firebase', 'firebase/app', 'firebase/auth', 'firebase/firestore']
+  }
 })
